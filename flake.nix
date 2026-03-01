ec30cc7a64839a04d19a3cf2a7355e8e70699fb4
André Lima <me@limwa.pt>
2026-03-01 18:09:13 +0000

    chore: add nix flake

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "A basic flake for Node.js development in Nix and NixOS";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:limwa/nix-flake-utils";
+
+    # Needed for shell.nix
+    flake-compat.url = "github:edolstra/flake-compat";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    ...
+  }:
+    utils.lib.mkFlakeWith {
+      forEachSystem = system: {
+        outputs = utils.lib.forSystem self system;
+
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      };
+    } {
+      formatter = {pkgs, ...}: pkgs.alejandra;
+
+      devShells = utils.lib.invokeAttrs {
+        default = {outputs, ...}: outputs.devShells.nodejs;
+
+        # Node.js development shell
+        nodejs = {pkgs, ...}:
+          pkgs.mkShell {
+            meta.description = "A development shell with Node.js v24";
+
+            packages = with pkgs; [
+              nodejs_24
+              corepack_24
+            ];
+          };
+      };
+    };
+}
